43da9ce0543cb0748217c688da0717a482563078
Will Dailey <willdailey14@MacBook-Pro-5.local>
2017-11-29 16:47:34 -0500

    Initial Commit

---
+++ MagicPorpoise/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MagicPorpoise
+//
+//  Created by Will Dailey on 11/29/17.
+//  Copyright © 2017 Will Dailey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
